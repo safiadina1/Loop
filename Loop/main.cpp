e7bc67bcc520082c410971734bb648698829184e
Safiullah <safiullahadina1@gmail.com>
2026-06-24 23:09:48 +0430

    Add files via upload

---
+++ Loop/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int num [2][3];
+ for (int i= 0; i< 2;i++){
+        for (int j= 0; j <3; j++){
+
+        }
+ }
+    cout<<"enter number"<<endl;
+    cin>>num [i][j];
+
+cout<<[0][0];
+
+    return 0;
+}
